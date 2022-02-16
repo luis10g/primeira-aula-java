b5fbd76499e46404160979083e1f70b1fb7fbdd7
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-02-16 17:51:53 +0000

    AULA-JAVA

--- Ola.java
+++ Ola.java
@@ -1,5 +1,9 @@
 public class Ola {
+    public static int soma(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!!");
+        System.out.println(soma(3, 5));
     }
 }
