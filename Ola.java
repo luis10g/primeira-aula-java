c92da1ebb05749766b097b9504b7703ce277c245
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-02-23 17:55:36 +0000

    AULA-JAVA

--- Ola.java
+++ Ola.java
@@ -5,5 +5,19 @@ public class Ola {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!!");
         System.out.println(soma(3, 5));
+
+
+        boolean bol = false;
+        char ch = 'a';
+        byte intB = 2;
+        short intS = 200;
+        int intN = 1000;
+        long intL = 2000;
+        float fNum = 2.5f;
+        double dNum = 2.5;
+
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
     }
 }
